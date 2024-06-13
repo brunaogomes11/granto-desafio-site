7e822fc2872cf49bc763b084b6686d9edab917d1
brunaogomes11 <brunogomesper@gmail.com>
2024-06-13 18:41:03 -0300

    a

--- app/app.py
+++ app/app.py
@@ -3,29 +3,15 @@ from flask import Flask, redirect, render_template, request, send_from_directory
 import os
 import configparser
 from pymongo import MongoClient, InsertOne
-<<<<<<< HEAD
 import requests
 
 app = Flask(__name__)
 url_base = 'https://granto-desafio-api.onrender.com'
 
-=======
-from bson import ObjectId, Binary
-import tempfile
-from dotenv import load_dotenv
-
-app = Flask(__name__)
-# Atualizando para buscar a URI corretamente
-database_url = os.getenv('DB_URI_PASS')
-client = MongoClient(database_url)
-db = client.mydata
-collection = db.mytable
->>>>>>> 1fa22a6a8a8de5f5dec049559b2b42055c4d4189
 @app.route("/")
 def homePage():
     return "Servidor Ok"
 
-<<<<<<< HEAD
 @app.route("/insercao")
 def insercao():
     return render_template("insercao.html")
@@ -154,109 +140,6 @@ def busca(query = '', pagina = 1):
         )
     else:
         return 405
-=======
-@app.route("/inserir")
-def inserir():
-    file = request.files['file']
-    other_data = request.form.to_dict()
-    id_objeto = ObjectId()
-    if file:
-        filename = file.filename
-        file_data = Binary(file.read())
-        # Insere o arquivo no MongoDB
-        document = {'_id':id_objeto, 'filename': filename, 'file_data': file_data}
-        # Adiciona outros dados ao documento
-        document.update(other_data)
-        collection.insert_one(document)
-        return jsonify({'message': 'Arquivo e dados guardados'})
-    else:
-        return jsonify({'message': 'Arquivo não encontrado'}), 404
-
-@app.route("/listar/<pagina>")
-@app.route("/listar/")
-@app.route("/listar")
-def listar(pagina=1):
-    total_documentos = collection.count_documents({})
-    page = int(pagina) if pagina else 1
-    page_size = 10
-    start_index = (page - 1) * page_size
-    num_pages = total_documentos // page_size + (1 if total_documentos % page_size > 0 else 0)
-    final_index = (start_index+10) if ((start_index+10) < total_documentos) else total_documentos
-    documentos = collection.find({}).skip(start_index).limit(page_size)
-    try:
-        result = []
-        for data in documentos:
-            data['_id'] = str(data['_id'])
-            if ('file_data' in data):
-                del data['file_data']
-            result.append(data)
-        return jsonify({'index_inicial':start_index, 'index_final':final_index,'total':total_documentos,'documentos': result, 'num_pages':num_pages})
-    except:
-        jsonify({"error":"Não foi possível encontrar nenhum dado"})
-
-@app.route('/quantidade_documentos')
-def quantidade_documentos():
-    try:
-        return jsonify({"Quantidade":collection.count_documents({})})
-    except:
-        return 404
-
-@app.route("/buscar/<pagina>/<query>/", methods=['GET', 'POST'])
-@app.route("/buscar/<pagina>/<query>", methods=['GET', 'POST'])
-@app.route("/buscar/<pagina>/", methods=['GET', 'POST'])
-@app.route("/buscar/<pagina>", methods=['GET', 'POST'])
-@app.route("/buscar/", methods=['GET', 'POST'])
-@app.route("/buscar", methods=['GET', 'POST'])
-def busca(query = '', pagina = None):
-    if request.method == "POST":
-        all_list = [' ', '', '*']
-        page = int(pagina) if pagina else 1
-        page_size = 10
-        start_index = (page - 1) * page_size
-        if query not in all_list:
-            index_config = {
-                "$search": {
-                    "index": "teste-search",
-                    "text": {
-                        "query": query,
-                        "path": {
-                            "wildcard": "*"
-                        }
-                    }
-                }
-            }
-            results = collection.aggregate([index_config])
-            formatted_results = []
-            for result in results:
-                result['_id'] = str(result['_id'])
-                if ('file_data' in result):
-                    del result['file_data']
-                formatted_results.append(result)
-            total_documentos = len(formatted_results)
-            num_pages = total_documentos // page_size + (1 if total_documentos % page_size > 0 else 0)
-            final_index = (start_index+10) if ((start_index+10) < total_documentos) else total_documentos
-            return jsonify({'index_inicial':start_index, 'index_final':final_index,'total':total_documentos,'documentos': formatted_results, 'num_pages':num_pages})
->>>>>>> 1fa22a6a8a8de5f5dec049559b2b42055c4d4189
-
-        elif query in all_list:
-            total_documentos = collection.count_documents({})
-            num_pages = total_documentos // page_size + (1 if total_documentos % page_size > 0 else 0)
-            final_index = (start_index+10) if ((start_index+10) < total_documentos) else total_documentos
-            documentos = collection.find({}).skip(start_index).limit(page_size)
-            try:
-                result = []
-                for data in documentos:
-                    data['_id'] = str(data['_id'])
-                    if ('file_data' in data):
-                        del data['file_data']
-                    result.append(data)
-                return jsonify({'index_inicial':start_index, 'index_final':final_index,'total':total_documentos,'documentos': result, 'num_pages':num_pages})
-            except:
-                jsonify({"error":"Não foi possível encontrar nenhum dado"}), 404
-        else:
-            return jsonify({"error": "Formulário Inválido"}), 400
-    else:
-        return jsonify({"error": "Método não permitido"}), 405
 
 @app.route("/baixar", methods=["GET", "POST"])
 def baixar():
